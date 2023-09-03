faccdf57d9dd8c9066dde520b41ee998f4b06880
Jonae <J0n4e@users.noreply.github.com>
2023-09-03 06:30:20 +0200

    version 1.0.1
    Added option to change color of the temperature and humidity icons

--- MMM-DHT22.js
+++ MMM-DHT22.js
@@ -1,14 +1,18 @@
 Module.register('MMM-DHT22', {
   defaults: {
     gpioPin: 6,
-    fontSize: '16px',
+    fontSize: '16px', // Default font size for all text
     fontFamily: 'Arial',
     showThermometerIcon: true,
     showDropletIcon: true,
     showTemperatureText: true,
     showHumidityText: true,
-    headerText: 'Local Temperature', // Default header text
-    updateInterval: 60, // Default update interval in seconds
+    headerText: 'Local Temperature',
+    updateInterval: 60,
+    temperatureIconColor: 'red',
+    humidityIconColor: 'blue',
+    temperatureFontSize: '', // Default: empty (inherits fontSize)
+    humidityFontSize: '',    // Default: empty (inherits fontSize)
   },
 
   start: function() {
@@ -16,8 +20,7 @@ Module.register('MMM-DHT22', {
     this.humidity = '';
     this.sendSocketNotification('START_DHT_READING', { gpioPin: this.config.gpioPin });
 
-    // Set the update interval based on the config
-    const updateIntervalMillis = this.config.updateInterval * 1000; // Convert seconds to milliseconds
+    const updateIntervalMillis = this.config.updateInterval * 1000;
     setInterval(() => {
       this.sendSocketNotification('START_DHT_READING', { gpioPin: this.config.gpioPin });
     }, updateIntervalMillis);
@@ -37,18 +40,15 @@ Module.register('MMM-DHT22', {
 
   getDom: function() {
     const wrapper = document.createElement('div');
-    wrapper.style.fontSize = this.config.fontSize;
-    wrapper.style.fontFamily = this.config.fontFamily;
+    wrapper.style.fontSize = this.config.fontSize; // Set the default font size
 
-    // Display header
     const header = document.createElement('div');
     header.style.fontWeight = 'bold';
     header.appendChild(document.createTextNode(this.config.headerText));
     wrapper.appendChild(header);
 
-    // Draw a horizontal line with thinner style
     const line = document.createElement('hr');
-    line.className = 'horizontal-line'; // Add a class for styling
+    line.className = 'horizontal-line';
     wrapper.appendChild(line);
 
     const temperatureDiv = document.createElement('div');
@@ -57,11 +57,18 @@ Module.register('MMM-DHT22', {
     }
     if (this.config.showThermometerIcon) {
       const thermometerIcon = document.createElement('span');
-      thermometerIcon.className = 'fa fa-thermometer-half'; // Font Awesome class
-      thermometerIcon.style.marginRight = '5px'; // Adjust spacing
+      thermometerIcon.className = 'fa fa-thermometer-half';
+      thermometerIcon.style.color = this.config.temperatureIconColor;
+      thermometerIcon.style.marginRight = '5px';
       temperatureDiv.appendChild(thermometerIcon);
     }
     temperatureDiv.appendChild(document.createTextNode(`${this.temperature}°C`));
+    
+    // Set the font size for temperature independently
+    if (this.config.temperatureFontSize) {
+      temperatureDiv.style.fontSize = this.config.temperatureFontSize;
+    }
+
     wrapper.appendChild(temperatureDiv);
 
     const humidityDiv = document.createElement('div');
@@ -70,11 +77,18 @@ Module.register('MMM-DHT22', {
     }
     if (this.config.showDropletIcon) {
       const dropletIcon = document.createElement('span');
-      dropletIcon.className = 'fa fa-tint'; // Font Awesome class
-      dropletIcon.style.marginRight = '5px'; // Adjust spacing
+      dropletIcon.className = 'fa fa-tint';
+      dropletIcon.style.color = this.config.humidityIconColor;
+      dropletIcon.style.marginRight = '5px';
       humidityDiv.appendChild(dropletIcon);
     }
     humidityDiv.appendChild(document.createTextNode(`${this.humidity}%`));
+
+    // Set the font size for humidity independently
+    if (this.config.humidityFontSize) {
+      humidityDiv.style.fontSize = this.config.humidityFontSize;
+    }
+
     wrapper.appendChild(humidityDiv);
 
     return wrapper;
